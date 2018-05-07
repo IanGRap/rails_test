f53e986caf9f19c9bd20c2b1afa09a68e0be2642
Ian Rapoport <irapopor@ucsc.edu>
2018-05-07 11:05:40 -0700

    added description column to project. migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507171624) do
+ActiveRecord::Schema.define(version: 20180507180445) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "description"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
