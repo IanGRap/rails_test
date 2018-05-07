fc648513db5d8474e51753e34dffb1c331379ae4
Ian Rapoport <irapopor@ucsc.edu>
2018-05-07 11:49:25 -0700

    added Users controller and root routes to a very basic form for making a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'users#new'
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  resources :users
 
 end
