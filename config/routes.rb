be9023816008d98186b163c418837a60d8514bb2
Ian Rapoport <irapopor@ucsc.edu>
2018-05-08 19:33:21 -0700

    project and task controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/new'
+
+  get 'projects/new'
+
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
