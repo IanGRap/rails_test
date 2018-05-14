47b0a2dfad6a29851ff1eff9f8103e6f0aa91a13
Ian Rapoport <irapopor@ucsc.edu>
2018-05-14 16:09:33 -0700

    Log in page up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'tasks/new'
 
   get 'projects/new'
@@ -7,10 +8,13 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'users#new'
+  root 'sessions#new'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   resources :users
 
