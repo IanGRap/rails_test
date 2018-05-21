0fe34f23230d14154e40423d8cde044b274783e0
Ian Rapoport <irapopor@ucsc.edu>
2018-05-21 14:07:06 -0700

    even more debug stuff

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,15 +3,19 @@ module ProjectsHelper
   include SessionsHelper
 
   def set_current_project(id)
-    @current_project = current_user.projects.find_by(id: params[:format])
+    @current_project = current_user.projects.find_by(id: id)
   end
 
   def current_project
     @current_project
   end
 
-  def project_set
+  def project_set?
     !@current_project.nil?
+  end
+
+  def clear_project
+    @current_project = nil
   end
 
 end
