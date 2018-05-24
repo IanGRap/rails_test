e975acaf18134aa7e3bd22959a94765b8c97f046
Ian Rapoport <irapopor@ucsc.edu>
2018-05-24 13:07:34 -0700

    updates to users routes, still working on update

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'tasks/new'
-
-  get 'projects/new'
-
-  get 'users/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'users#new'
 
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  get '/new_user', to: 'users#new'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/user/:id/edit', to: 'users#edit', as: 'edit_user'
+  post '/new_user', to: 'users#create'
+  put '/users/:id', to: 'users#update'
+  patch '/users/:id', to: 'users#update'
 
   get '/project', to: 'projects#new'
   post '/project', to: 'projects#create'
@@ -19,8 +17,8 @@ Rails.application.routes.draw do
   get '/task', to: 'tasks#new'
   post '/task', to: 'tasks#create'
 
-  resources :users
-  resources :projects
-  resources :tasks
+  #resources :users
+  #resources :projects
+  #resources :tasks
 
 end
