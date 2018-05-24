2f8cc541d85bab4c6bc8edf1e7c2f26444947268
Ian Rapoport <irapopor@ucsc.edu>
2018-05-24 12:13:41 -0700

    sessions routes and links removed

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'sessions#new'
+  root 'users#new'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'session#destroy'
 
   get '/project', to: 'projects#new'
   post '/project', to: 'projects#create'
